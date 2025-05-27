6559d7491d04a9f4564434e165f58d2a3821c14b
Hemank <betterartyish24@gmail.com>
2025-05-27 17:28:14 +0530

    testing model

--- my-fastapi-app/predict.py
+++ my-fastapi-app/predict.py
@@ -7,7 +7,7 @@ import joblib
 
 
 encoder = joblib.load('my-fastapi-app/label_encoder.joblib')
-model = load_model('my-fastapi-app/mainmodel.h5')
+model = load_model('main_model')
 def predict_doodle(img_array):
     img_resized = cv2.resize(img_array, (96, 96))
     img_normalized = img_resized / 255.0
