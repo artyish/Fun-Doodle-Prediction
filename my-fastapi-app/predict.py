8dc0d13bec4b872673bca88eb3a39a8d4a3538c7
Hemank <betterartyish24@gmail.com>
2025-05-27 20:06:30 +0530

    update model loading

--- my-fastapi-app/predict.py
+++ my-fastapi-app/predict.py
@@ -5,11 +5,16 @@ import os
 from sklearn.preprocessing import LabelEncoder
 import joblib
 
-
+def get_model():
+    global _model
+    if _model is None:
+        print("Loading model lazily...")
+        _model = tf.keras.models.load_model("main_model")
+    return _model
 
 def predict_doodle(img_array):
     encoder = joblib.load('label_encoder.joblib')
-    model = load_model('main_model')
+    model = get_model()
     img_resized = cv2.resize(img_array, (96, 96))
     img_normalized = img_resized / 255.0
     img_array_to_use = img_normalized.reshape(1, 96, 96, 1)
