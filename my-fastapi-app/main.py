cda8de63b88e9579a55ce46517493da3e696ca10
Hemank <betterartyish24@gmail.com>
2025-05-27 20:52:03 +0530

    correct problem in main.py

--- my-fastapi-app/main.py
+++ my-fastapi-app/main.py
@@ -3,4 +3,4 @@ import uvicorn
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:api", host="0.0.0.0", port=port, log_level="info")
+    uvicorn.run("api:api", host="0.0.0.0", port=port, log_level="info")
