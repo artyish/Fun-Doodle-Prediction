67cc42fd77abb9705017a5fcb3045ce2ca37b9b5
Hemank <betterartyish24@gmail.com>
2025-05-27 12:43:43 +0530

    add fastapi app

---
+++ my-fastapi-app/api.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI , File, UploadFile
+from predict import predict_doodle
+from PIL import Image
+from io import BytesIO
+import numpy as np
+from fastapi.middleware.cors import CORSMiddleware
+
+
+#origins = [
+#    "http://localhost:3000",  # your frontend URL
+#    "http://127.0.0.1:3000",
+#    # add other origins you want to allow
+#]
+
+api = FastAPI()
+
+# GET - to get information
+# POST - submitting new information
+# PUT - updating information 
+# DELETE - deleting information
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@api.get('/')
+def index():
+    return {"message":"Hello World"}
+
+
+@api.post('/predict')
+async def predict(file: UploadFile = File(...)):
+    content = await file.read() # wait for the file to be read first
+    image = Image.open(BytesIO(content)).convert('L') # bytesIO to handle bytes 
+    img_array = np.array(image) # because it is converted through PIL we have to convert it to nparray
+    get_response = predict_doodle(img_array) # send it to the other file
+    return get_response
+
+
---
+++ my-fastapi-app/predict.py
@@ -0,0 +1,28 @@
+from tensorflow.keras.models import load_model
+import cv2
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+import joblib
+
+encoder = joblib.load('label_encoder.joblib')
+
+model = load_model("main_model")
+
+def predict_doodle(img_array):
+    img_resized = cv2.resize(img_array, (96, 96))
+    img_normalized = img_resized / 255.0
+    img_array_to_use = img_normalized.reshape(1, 96, 96, 1)
+
+    prediction = model.predict(img_array_to_use)[0] # because our predictions our actually returned as (1 , number_of_classes)
+    prediction_sorted = np.argsort(prediction)[::-1][:3] # pick last 3 using argsort / argsort actually returns the index instead of the prediction which is pretty cool
+    print(prediction_sorted)
+    top_3_probabilities = []
+    for i in prediction_sorted:
+        label = encoder.inverse_transform([i])[0]
+        probability = prediction[i] * 100
+        top_3_probabilities.append((label, probability))
+
+    return top_3_probabilities
+    
+
+
