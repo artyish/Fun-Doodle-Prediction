c1b6995c9ab6a2c26a526e02fa44a27bd6c9d90f
Hemank <betterartyish24@gmail.com>
2025-05-27 21:57:47 +0530

    update api.py

--- my-fastapi-app/api.py
+++ my-fastapi-app/api.py
@@ -21,7 +21,7 @@ api = FastAPI()
 
 api.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:3000","http://localhost:3000"],
+    allow_origins=["http://127.0.0.1:3000","http://localhost:3000""],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
